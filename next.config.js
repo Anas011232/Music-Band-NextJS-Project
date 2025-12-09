18d72445f0a8b21619f74eef7f79ed4163c78687
Anas011232 <nulltohero@gmail.com>
2025-12-10 04:22:45 +0600

    Completed

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Netlify serverless deploy er jonno
+  reactStrictMode: true, // optional
+};
+
+module.exports = nextConfig;
